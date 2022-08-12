05d054f7272b0109bb0d7aca9a2270b4302030a5
fiveFloorMan <harvieusegithub@gmail.com>
2022-08-12 16:10:14 +0800

    初始設定

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
