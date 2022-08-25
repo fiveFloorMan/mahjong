870d698ffae050666e8d322fc545aa6962e83b91
fiveFloorMan <harvieusegithub@gmail.com>
2022-08-25 16:06:27 +0800

    mongodb connection setting complete

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 3000
+
+mongoose.connect('mongodb+srv://harviehung:mahjong@mahjong.uo9sd2f.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.get('/', (req, res) => {
   res.render('index')
