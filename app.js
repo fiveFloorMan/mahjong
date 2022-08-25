9e4428b9511ea9573a6efca1c7c71350c186d278
fiveFloorMan <harvieusegithub@gmail.com>
2022-08-25 14:38:46 +0800

    初始化設定

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
 const app = express()
 const PORT = 3000
 
-const exphbs = require('express-handlebars')
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
-
-
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
