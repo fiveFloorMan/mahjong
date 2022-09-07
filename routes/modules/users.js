8b476877be5dd7723f8adea47dd6c3e229747b46
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-07 18:09:28 +0800

    login 頁面完成

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+// 註冊
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
 })
 
 module.exports = router
