2d3a6d9fcd682d1a2e100c05424f479e7f1e0bb1
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-10 03:43:59 +0800

    增加新增功能

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,13 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record.js')
+
+// 新增紀錄
 router.get('/addRecord', (req, res) => {
   res.render('addRecord')
 })
 
 router.post('/addRecord', (req, res) => {
-  const record = req.body
-  console.log('req.body', req.body)
+  const newRecord = req.body  
+  Record.create({
+    playerName: newRecord.playerName,
+    score: newRecord.score,
+    gameTimes: newRecord.gameTimes,
+    date: newRecord.date
+  })
+  // 可以再做一個提示新增成功的優化
+  res.redirect('/admin/addRecord')
 })
 
 module.exports = router
+
