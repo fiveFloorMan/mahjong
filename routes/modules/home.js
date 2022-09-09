1b7f22bfd7fab192b72d3aaddb9de16585b54414
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-09 22:16:48 +0800

    完成Rank Data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,18 +7,19 @@ const Record = require('../../models/record.js') // 整包資料
 const allPlayerName = [] 
 
 router.get('/', (req, res) => {
-  Record.find()
+  Record.find() //非同步
     .lean()
     .then(Data => {
       // 拿出playerName
       pushAllPlayerName(Data)
       let filterPlayerName = allPlayerName.filter(function(playerName, index, self){
         return self.indexOf(playerName) === index;
-      });
+      })
       
       // 整理相同姓名(playerName)的成績(Score) & 降數(gameTimes)
       const scoreArray = []
-      const gameTimeArray = []
+      const gameTimesArray = []
+
       filterPlayerName.map(function(name){
         let score = 0
         let gameTimes = 0
@@ -29,21 +30,21 @@ router.get('/', (req, res) => {
           }
         })
         scoreArray.push(score)
-        gameTimeArray.push(gameTimes)
+        gameTimesArray.push(gameTimes)
       })
 
       // 組成一個Array
       const playerData = filterPlayerName.map((name, index) => ({ 
         playerName: name,
         score: scoreArray[index],
-        gameTimes: gameTimeArray[index]
+        gameTimes: gameTimesArray[index]
       }))
 
       //排序
       playerData.sort(function(a, b) {
         return b.score - a.score //降冪
       })
-      
+
       // 加入排行數字
       let rankNumber = []
       for(let i = 1; i <= playerData.length; i++){
@@ -52,8 +53,8 @@ router.get('/', (req, res) => {
       const playerDataRank = rankNumber.map((rank, index) => ({
         rank: rank,
         playerName: playerData[index].playerName,
-        score: scoreArray[index],
-        gameTimes: gameTimeArray[index]
+        score: playerData[index].score,
+        gameTimes: playerData[index].gameTimes
       }))
     res.render('index', { playerDataRank })
     })
