1541143b54b42a4f4a3954fbef9b397a17241c55
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-10 03:15:31 +0800

    頁面完成

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record.js')
 router.get('/addRecord', (req, res) => {
   res.render('addRecord')
 })
 
-
+router.post('/addRecord', (req, res) => {
+  
+})
 
 module.exports = router
