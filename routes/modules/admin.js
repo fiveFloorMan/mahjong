63ec4182b1c3d7e36ece1c5a4075d0f25627c759
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-10 00:43:54 +0800

    add record routes

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/')
+router.get('/addRecord', (req, res) => {
+  res.render('addRecord')
+})
+
+
 
 module.exports = router
