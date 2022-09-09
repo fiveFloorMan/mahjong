8afcd1ecc7e5ac4289bc4d7fb42ad14804f8ff6a
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-10 00:38:50 +0800

    add admin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const users = require('./modules/users')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.use('/users', users)
 router.use('/', home)
 
