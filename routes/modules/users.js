9a71fdd9dac758d2f1e1fd9c8a650d9ee51607ab
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-10 20:06:55 +0800

    中斷點

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+const Player = require('../../models/player')
 // 註冊
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const registerData = req.body
+  Player.create({
+    
+  })
 })
 
 router.get('/login', (req, res) => {
