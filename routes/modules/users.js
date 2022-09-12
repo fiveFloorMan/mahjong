0fc892c9c2b809fbd217db598b9fbf68bfc99c4c
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-12 15:25:07 +0800

    message

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,13 +8,34 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const registerData = req.body
-  Player.create({
-    name: registerData.name,
-    password: registerData.password,
-    experience: registerData.experience
-  })
-  res.redirect('/users/login')
+  const { name, password, confirmPassword, experience } = req.body
+
+  // register 過程中的錯誤辨認
+  const errors = []
+  // 有沒有填寫的欄位
+  if ( !name || !password || !confirmPassword ) {
+    errors.push({ message: '所有欄位都是必填的'})
+  }
+  // password 和 confirmPassword 是否相同
+  if ( password !== confirmPassword ) {
+    errors.push({ message: '密碼和確認密碼不相符'})
+  }
+  
+  if (errors.length) {
+    return res.render('register', { errors })
+  }
+  // name 是否已經被註冊過
+  const registeredName = Player.findOne({ name })
+  if (registeredName) {
+    errors.push({ message: '這個名稱已經被註冊過了'})
+  }  
+
+
+  if (!errors.length){
+    Player.create({ name, password, experience })
+    res.redirect('/users/login')
+  }
+  
 })
 
 // 登入
