44170b73a211734b7958fb43dcb00844aa40d53e
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-14 17:01:06 +0800

    會員驗證

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,12 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+const { authenticator } = require('../middleware/auth')
+
+router.use('/admin', authenticator, admin)
 router.use('/users', users)
 router.use('/', home)
+
+
 
 module.exports = router
