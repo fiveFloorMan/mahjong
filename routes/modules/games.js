25abff82ea8471ed1c6c5c72734688f4d8c03a4f
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-16 17:14:30 +0800

    search routes & search select

--- routes/modules/games.js
+++ routes/modules/games.js
@@ -1,10 +1,45 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record')
 
-// 個人資料查詢
+// 單筆資料查詢(全體)
 router.get('/playerScore', (req, res) => {
-  res.render('gamesPlayerScore')
+  Record.find()
+    .lean()
+    .then(Data => {
+      // 抓出playerName,作為select選項且不重複
+      const playerNameArray = []
+      for(let i = 0; i < Data.length; i++){
+        playerNameArray.push(Data[i].playerName)
+      }
+      const playerNameArraySet = new Set(playerNameArray)
+
+      // sort
+      Data.sort((a, b) => {
+        return new Date(a.date) - new Date(b.date)
+      })
+      return res.render('gamesPlayerScore', { Data, playerNameArraySet })
+    })
+})
+
+// 單筆資料查詢(單人)
+router.get('/playerScore/search', (req, res) => {
+  const { keyPlayerName } = req.query
+  Record.find({ playerName: keyPlayerName })
+    .lean()
+    .then(Data => {
+      let totalScore = 0
+      let totalGameTimes = 0
+      Data.map(data => {
+        totalScore += data.score
+        totalGameTimes += data.gameTimes
+      })
+      Data.sort((a, b) => {
+        return new Date(a.date) - new Date(b.date)
+      })
+      return res.render('SearchGamesPlayerScore', { Data, totalScore, totalGameTimes, keyPlayerName })
+    })
 })
 
 
