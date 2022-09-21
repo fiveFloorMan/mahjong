5649613349124e312fc6d9b74da2612b8d0d16df
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-21 19:47:16 +0800

    routes /games need authenticator

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const games = require('./modules/games')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/admin', authenticator, admin)
-router.use('/games', games)
+router.use('/games', authenticator, games)
 router.use('/users', users)
 router.use('/', home)
 
