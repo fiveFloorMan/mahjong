2004797a2821647b42e31a899ffda78fc9860e08
fiveFloorMan <harvieusegithub@gmail.com>
2022-09-22 17:45:21 +0800

    add dotenv

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
